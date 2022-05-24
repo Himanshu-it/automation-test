3db8bbd4521f8c8ef82fc6f2a321ac68df6c0f1d
Himanshu Mishra <hmdcisin@gmail.com>
2022-05-24 15:38:00 +0530

    Added secret to workflow..

--- simple_test.py
+++ simple_test.py
@@ -2,5 +2,6 @@ import os
 
 
 env = os.getenv('TEST_ENV')
+token_auth = os.getenv('TOKEN_AUTH_ENV')
 def test_create_primerx_patient():
-	print('Running tests for env : ' + env)
+	print('Running tests for env : ' + env + ' token is : ' + token_auth)
