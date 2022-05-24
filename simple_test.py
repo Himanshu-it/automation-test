77c9462c00ff171d6a599313e0cb575ce01bb405
Himanshu Mishra <hmdcisin@gmail.com>
2022-05-24 16:03:01 +0530

    Added chect for empty secret.

--- simple_test.py
+++ simple_test.py
@@ -4,4 +4,8 @@ import os
 env = os.getenv('TEST_ENV')
 token_auth = os.getenv('TOKEN_AUTH_ENV')
 def test_create_primerx_patient():
+	if (not token_auth):
+		print('Token auth is empty ')
+	else:
+		print('Token auth is not empty ')
 	print('Running tests for env : ' + env + ' token is : ' + token_auth)
